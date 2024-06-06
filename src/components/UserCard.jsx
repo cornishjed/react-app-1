95cbbd5827a74adc2ddcb8000d75842b69616df1
cornishjed <judgejed@gmail.com>
2024-06-06 21:39:58 +0100

    refactor

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './UserCard.css';
+
 export default function UserCard({key, name, email, avatar}) {
     return (
-        <li className='userslist__listitem'>
+        <li className='usercard'>
             <h4>{name}</h4>
             <i>{email}</i>
             <img src={avatar} alt={name}/>
